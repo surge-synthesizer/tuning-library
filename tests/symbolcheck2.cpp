ddb9cb0aad741395a063bcde02085db35ba74651
Paul Walker <paul@pwjw.com>
2021-07-01 14:24:07 -0400

    Adopt the recent Surge clang-format
    Adopt the same clang-format file as surge
    Format conssitently

--- tests/symbolcheck2.cpp
+++ tests/symbolcheck2.cpp
@@ -7,13 +7,14 @@
 #include <iomanip>
 #include "Tunings.h"
 
-double symbolcheck2() {
-   auto k = Tunings::tuneNoteTo( 60, 200 );
-   return k.tuningFrequency;
+double symbolcheck2()
+{
+    auto k = Tunings::tuneNoteTo(60, 200);
+    return k.tuningFrequency;
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-   extern double symbolcheck1();
-   std::cout << "100 and 200 are " << symbolcheck1() << " and " << symbolcheck2 () << std::endl;
+    extern double symbolcheck1();
+    std::cout << "100 and 200 are " << symbolcheck1() << " and " << symbolcheck2() << std::endl;
 }
